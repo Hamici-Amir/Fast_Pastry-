feda624f69a06037362547d61ff44c95869c3cf7
Fast Pastry Dev <dev@fastpastry.com>
2026-06-16 12:28:27 +0200

    add LanguagePicker to HomeHeader

--- src/components/home/HomeHeader.tsx
+++ src/components/home/HomeHeader.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { AppHeader } from '../common/AppHeader';
+import { LanguagePicker } from '../ui/LanguagePicker';
 
 export const HomeHeader: React.FC = () => {
   const { t } = useTranslation();
@@ -12,6 +13,7 @@ export const HomeHeader: React.FC = () => {
       showAvatar
       hasNotifications
       avatarUri="https://images.unsplash.com/photo-1494790108377-be9c29b29330?q=80&w=100&auto=format&fit=crop"
+      rightContent={<LanguagePicker compact />}
     />
   );
 };
